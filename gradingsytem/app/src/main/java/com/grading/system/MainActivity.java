9a75eab20cead425d637a18dcd3218461aee2dec
Syed-Bipul-Rahman <syedbipulrahman2@gmail.com>
2023-08-23 01:19:34 +0600

    ui updated

--- gradingsytem/app/src/main/java/com/grading/system/MainActivity.java
+++ gradingsytem/app/src/main/java/com/grading/system/MainActivity.java
@@ -10,5 +10,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //change status bar color
+        getWindow().setStatusBarColor(getResources().getColor(R.color.orange));
+        //change support action bar color
+       getSupportActionBar().setBackgroundDrawable(getResources().getDrawable(R.drawable.actionbar_background));
+
     }
 }

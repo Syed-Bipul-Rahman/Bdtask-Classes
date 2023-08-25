2b339ccf94f5e7e66248e55a1b894321dffdd590
Syed-Bipul-Rahman <syedbipulrahman2@gmail.com>
2023-08-25 23:48:45 +0600

    fibonacci number added

---
+++ Fibonacci/app/src/main/java/com/storedata/fibonacci/MainActivity.java
@@ -0,0 +1,56 @@
+package com.storedata.fibonacci;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.storedata.fibonacci.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+//view binding object
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding=ActivityMainBinding.inflate(getLayoutInflater());
+        View view=binding.getRoot();
+        setContentView(view);
+
+        //after clicking button fibbonacci series will be shown
+        binding.btnget.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int n = Integer.parseInt(binding.editTextgetnum.getText().toString());
+                String fiboSeries = generateFibonacci(n);
+                binding.showmsg.setText(fiboSeries);
+            }
+        });
+    }
+
+    private String generateFibonacci(int n) {
+        if (n <= 0) {
+            return "Invalid input";
+        }
+
+        StringBuilder result = new StringBuilder();
+        int first = 0, second = 1;
+
+        result.append(first).append(", "); // Add the first two Fibonacci numbers
+
+        for (int i = 2; i < n; i++) {
+            int next = first + second;
+            result.append(next);
+            if (i != n - 1) {
+                result.append(", ");
+            }
+
+            first = second;
+            second = next;
+        }
+
+        return result.toString();
+    }
+}

c2309a11eeda5277b1d30d1c14758ef2f26888b8
Syed-Bipul-Rahman <syedbipulrahman2@gmail.com>
2023-08-27 01:59:48 +0600

    sharedprefences added and logic updated, validation added

--- gradingsytem/app/src/main/java/com/grading/system/ActivityResult.java
+++ gradingsytem/app/src/main/java/com/grading/system/ActivityResult.java
@@ -3,6 +3,8 @@ package com.grading.system;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
@@ -10,15 +12,15 @@ import android.widget.Toast;
 import com.grading.system.databinding.ActivityResultBinding;
 
 public class ActivityResult extends AppCompatActivity {
-private ActivityResultBinding binding;
+    private ActivityResultBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        binding=ActivityResultBinding.inflate(getLayoutInflater());
+        binding = ActivityResultBinding.inflate(getLayoutInflater());
 
-        View view=binding.getRoot();
+        View view = binding.getRoot();
 
         setContentView(view);
 
@@ -30,116 +32,99 @@ private ActivityResultBinding binding;
         getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN);
 
 
+        //if user click set custom values button then data will deleted from sharedpreferescess and
+        // navigate to first page
 
-//        if (binding.usermark.getText().toString().isEmpty()) {
-//            Toast.makeText(this, "You cannot leave the field Empty", Toast.LENGTH_SHORT).show();
-//            binding.usermark.setError("Please enter your mark");
-//            binding.usermark.requestFocus();
-//            return;
-//        } else if (Integer.parseInt(binding.usermark.getText().toString()) > 100) {
-//            Toast.makeText(this, "Mark cannot greater than 100", Toast.LENGTH_LONG).show();
-//            binding.usermark.setError("Please enter a valid mark");
-//            binding.usermark.requestFocus();
-//            return;
-//        } else if (Integer.parseInt(.getText().toString()) < 0) {
-//            Toast.makeText(this, "Mark cannot less than zero", Toast.LENGTH_LONG).show();
-//            binding.usermark.setError("Please enter a valid mark");
-//            binding.usermark.requestFocus();
-//            return;
-//        }
-//        //at the end of validation check the result
-//
-//        else {
-//            if (Integer.parseInt(binding.usermark.getText().toString()) >= Integer.parseInt(ap.getText().toString())) {
-//
-//                AlertDialog.Builder builder = new AlertDialog.Builder(this);
-//                builder.setTitle("Congratulations");
-//                builder.setMessage("Your Grade is A+");
-//                builder.setPositiveButton("OK", (dialog, which) -> {
-//                    dialog.dismiss();
-//                });
-//
-//                AlertDialog alertDialog = builder.create();
-//                alertDialog.show();
-//
-//            } else if (Integer.parseInt(binding.usermark.getText().toString()) >= Integer.parseInt(a.getText().toString())) {
-//
-//                AlertDialog.Builder builder = new AlertDialog.Builder(this);
-//                builder.setTitle("Result");
-//                builder.setMessage("Your Grade is A");
-//                builder.setPositiveButton("OK", (dialog, which) -> {
-//                    dialog.dismiss();
-//                });
-//
-//                AlertDialog alertDialog = builder.create();
-//                alertDialog.show();
-//
-//            } else if (Integer.parseInt(.getText().toString()) >= Integer.parseInt(bp.getText().toString())) {
-//
-//                AlertDialog.Builder builder = new AlertDialog.Builder(this);
-//                builder.setTitle("Result");
-//                builder.setMessage("Your Grade is B+");
-//                builder.setPositiveButton("OK", (dialog, which) -> {
-//                    dialog.dismiss();
-//                });
-//
-//                AlertDialog alertDialog = builder.create();
-//                alertDialog.show();
-//
-//            } else if (Integer.parseInt(.getText().toString()) >= Integer.parseInt(b.getText().toString())) {
-//
-//                AlertDialog.Builder builder = new AlertDialog.Builder(this);
-//                builder.setTitle("Result");
-//                builder.setMessage("Your Grade is B");
-//                builder.setPositiveButton("OK", (dialog, which) -> {
-//                    dialog.dismiss();
-//                });
-//
-//                AlertDialog alertDialog = builder.create();
-//                alertDialog.show();
-//
-//            } else if (Integer.parseInt(.getText().toString()) >= Integer.parseInt(c.getText().toString())) {
-//
-//                AlertDialog.Builder builder = new AlertDialog.Builder(this);
-//                builder.setTitle("Result");
-//                builder.setMessage("Your Grade is C");
-//                builder.setPositiveButton("OK", (dialog, which) -> {
-//                    dialog.dismiss();
-//                });
-//
-//                AlertDialog alertDialog = builder.create();
-//                alertDialog.show();
-//
-//            } else if (Integer.parseInt(.getText().toString()) >= Integer.parseInt(d.getText().toString())) {
-//
-//                AlertDialog.Builder builder = new AlertDialog.Builder(this);
-//                builder.setTitle("Result");
-//                builder.setMessage("Your Grade is D");
-//                builder.setPositiveButton("OK", (dialog, which) -> {
-//                    dialog.dismiss();
-//                });
-//
-//                AlertDialog alertDialog = builder.create();
-//                alertDialog.show();
-//
-//            } else if (Integer.parseInt(.getText().toString()) < Integer.parseInt(d.getText().toString())) {
-//
-//                AlertDialog.Builder builder = new AlertDialog.Builder(this);
-//                builder.setTitle("Alas!");
-//
-//
-//                builder.setMessage("You Are Failed");
-//                builder.setPositiveButton("OK", (dialog, which) -> {
-//                    dialog.dismiss();
-//                });
-//
-//                AlertDialog alertDialog = builder.create();
-//                alertDialog.show();
-//
-//            }
-//        }
+        binding.setcustomvalues.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //deleting data from shared preferences
+                SharedPreferences preferences = getSharedPreferences("MyPrefsFile", MODE_PRIVATE);
+                SharedPreferences.Editor editor = preferences.edit();
+                editor.clear();
+                editor.apply();
+                Toast.makeText(ActivityResult.this, "Data deleted", Toast.LENGTH_SHORT).show();
+                //navigate to first page
+                startActivity(new Intent(ActivityResult.this, MainActivity.class));
+                finish();
+            }
+        });
 
 
+//if user clicked on check gpa button
+
+        binding.checkresult.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String userMarkStr = binding.usermark.getText().toString().trim();
+
+                if (userMarkStr.isEmpty()) {
+                    binding.usermark.setError("Enter valid mark");
+                    binding.usermark.requestFocus();
+                    return;
+                }
+
+                //get shared preferencess
+                SharedPreferences preferences = getSharedPreferences("MyPrefsFile", MODE_PRIVATE);
+
+//get data from shared preferences
+                String as = preferences.getString("a", "");
+                //convert to int for calculation
+                int a = Integer.parseInt(as);
+
+                String aps = preferences.getString("ap", "");
+                int ap = Integer.parseInt(aps);
+                String bps = preferences.getString("bp", "");
+                int bp = Integer.parseInt(bps);
+                String bs = preferences.getString("b", "");
+                int b = Integer.parseInt(bs);
+                String cs = preferences.getString("c", "");
+                int c = Integer.parseInt(cs);
+                String ds = preferences.getString("d", "");
+                int d = Integer.parseInt(ds);
+
+
+                //get user input from edit text
+                int usermarks = Integer.parseInt(binding.usermark.getText().toString());
+
+
+                //check user input is valid or not
+
+                if (usermarks > 100 || usermarks < 0) {
+                    binding.usermark.setError("Enter valid mark");
+                    binding.usermark.requestFocus();
+
+                } else {
+
+                    if (usermarks >= ap) {
+                        showResultDialog("Congratulations", "Your Grade is A+");
+                    } else if (usermarks >= a) {
+                        showResultDialog("Congratulations", "Your Grade is A");
+                    } else if (usermarks >= bp) {
+                        showResultDialog("Congratulations", "Your Grade is B+");
+                    } else if (usermarks >= b) {
+                        showResultDialog("Congratulations", "Your Grade is B");
+                    } else if (usermarks >= c) {
+                        showResultDialog("Congratulations", "Your Grade is C");
+                    } else if (usermarks >= d) {
+                        showResultDialog("Congratulations", "Your Grade is D");
+                    } else {
+                        showResultDialog("Oops", "You are fail");
+                    }
+                }
+            }
+        });
+    }
+
+    private void showResultDialog(String title, String message) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(ActivityResult.this);
+        builder.setTitle(title);
+        builder.setMessage(message);
+        builder.setPositiveButton("OK", (dialog, which) -> {
+            dialog.dismiss();
+        });
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
     }
 
 
--- gradingsytem/app/src/main/java/com/grading/system/MainActivity.java
+++ gradingsytem/app/src/main/java/com/grading/system/MainActivity.java
@@ -35,24 +35,53 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().setBackgroundDrawable(getResources().getDrawable(R.drawable.actionbar_background));
 
 
-        //initialization of variables
+        // Get SharedPreferences instance
+        SharedPreferences preferences = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
+        // Check if data is already saved
+        boolean isDataSaved = preferences.contains("a");
+        if (isDataSaved) {
+//                // Data is already saved, navigate to the next page
 
+            startActivity(new Intent(MainActivity.this, ActivityResult.class));
+            finish();
+        }
 
         binding.setvalue.setOnClickListener(v -> {
-            //call the method to check the result
+            //getting data from edit text
+            String a = binding.a.getText().toString();
+            String ap = binding.ap.getText().toString();
+            String b = binding.b.getText().toString();
+            String bp = binding.bp.getText().toString();
+            String c = binding.c.getText().toString();
+            String d = binding.d.getText().toString();
 
-            // Get SharedPreferences instance
-            SharedPreferences preferences = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
+            if (a.isEmpty() || ap.isEmpty() || b.isEmpty() || bp.isEmpty() || c.isEmpty() || d.isEmpty()) {
 
-            // Check if data is already saved
-            boolean isDataSaved = preferences.contains("username"); // Assuming "username" is a key you use to save data
 
-            if (isDataSaved) {
-//                // Data is already saved, navigate to the next page
-//                Intent intent = new Intent(this, NextActivity.class); // Replace NextActivity with the actual next activity
-//                startActivity(intent);
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("Error");
+                builder.setMessage("Please fill all the fields");
+                builder.setPositiveButton("OK", null);
+                builder.show();
+            } else {
+
+
+                // Saving data
+                SharedPreferences.Editor editor = preferences.edit();
+                editor.putString("ap", ap);
+                editor.putString("a", a);
+
+                editor.putString("bp", bp);
+                editor.putString("b", b);
+                editor.putString("c", c);
+
+                editor.putString("d", d);
+
+                editor.apply();
+//sent to next activity
                 startActivity(new Intent(MainActivity.this, ActivityResult.class));
-                finish(); // Close the current activity
+                finish();
+
             }
 
 

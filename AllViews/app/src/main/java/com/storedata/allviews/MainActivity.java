418743f735a6df970bf152095967f6e2468f9994
Syed-Bipul-Rahman <syedbipulrahman2@gmail.com>
2023-08-29 23:45:28 +0600

    viewbinding

--- AllViews/app/src/main/java/com/storedata/allviews/MainActivity.java
+++ AllViews/app/src/main/java/com/storedata/allviews/MainActivity.java
@@ -3,12 +3,19 @@ package com.storedata.allviews;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.storedata.allviews.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-
+ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+
+        View view = binding.getRoot();
+        setContentView(view);
     }
 }

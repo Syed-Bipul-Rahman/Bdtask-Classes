0762007bbbe8d30bab5e924a182549dbea61311c
Syed-Bipul-Rahman <syedbipulrahman2@gmail.com>
2023-09-02 21:59:51 +0600

    fragment and viewpager added

--- AllViews/app/src/main/java/com/storedata/allviews/ContainersActivity.java
+++ AllViews/app/src/main/java/com/storedata/allviews/ContainersActivity.java
@@ -1,14 +1,17 @@
 package com.storedata.allviews;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.android.material.tabs.TabLayout;
 import com.storedata.allviews.databinding.ActivityContainersBinding;
 
 public class ContainersActivity extends AppCompatActivity {
     ActivityContainersBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,5 +19,19 @@ public class ContainersActivity extends AppCompatActivity {
 
         View view = binding.getRoot();
         setContentView(view);
+
+        TabLayout tabLayout = binding.tabslay;
+        ViewPager viewPager = binding.viewpager;
+
+        tabLayout.addTab(tabLayout.newTab().setText("People"));
+
+
+        tabLayout.addTab(tabLayout.newTab().setText("Call"));
+        MyPagerAdapter pagerAdapter = new MyPagerAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(pagerAdapter);
+
+        // Connect the TabLayout with the ViewPager.
+        tabLayout.setupWithViewPager(viewPager);
+
     }
 }
---
+++ AllViews/app/src/main/java/com/storedata/allviews/MyPagerAdapter.java
@@ -0,0 +1,50 @@
+package com.storedata.allviews;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+public class MyPagerAdapter extends FragmentPagerAdapter {
+    private static final int NUM_PAGES = 3;
+
+    public MyPagerAdapter(@NonNull FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        // Return the fragment associated with the specified position
+        switch (position) {
+            case 0:
+                return new CallFragment();
+            case 1:
+                return new PeopleFragment();
+            case 2:
+                return new HistoryFragment();
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+
+        return NUM_PAGES;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        // Return the title of each tab
+        switch (position) {
+            case 0:
+                return "Call";
+            case 1:
+                return "People";
+            case 2:
+                return "History";
+            default:
+                return "";
+        }
+    }
+}

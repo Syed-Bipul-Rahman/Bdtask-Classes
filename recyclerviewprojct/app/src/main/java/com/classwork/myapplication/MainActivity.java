1b47cc9d28e64a16a2b711e4b00657e392b60367
Syed-Bipul-Rahman <syedbipulrahman2@gmail.com>
2023-09-05 22:35:11 +0600

    Recyclerview implement

--- recyclerviewprojct/app/src/main/java/com/classwork/myapplication/MainActivity.java
+++ recyclerviewprojct/app/src/main/java/com/classwork/myapplication/MainActivity.java
@@ -1,14 +1,43 @@
 package com.classwork.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+    ArrayList<Model> arrayList;
+    RecyclerView recyclerView;
+    Adapter recyclerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerData();
+
+        recyclerView = findViewById(R.id.recyclerviewmain);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        linearLayoutManager.setOrientation(RecyclerView.VERTICAL);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+
+        recyclerAdapter = new Adapter(this, arrayList);
+        recyclerView.setAdapter(recyclerAdapter);
+
+
+    }
+
+    private void recyclerData() {
+        arrayList = new ArrayList<>();
+        arrayList.add(new Model(R.drawable.ic_splash, "This is simple text"));
+        arrayList.add(new Model(R.drawable.ic_splash, "This is simple text"));
+        arrayList.add(new Model(R.drawable.ic_splash, "This is simple text"));
+
+
     }
 }

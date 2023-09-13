f708a0bc034e36b1f64cb93e478bd2c1037514f5
Syed-Bipul-Rahman <syedbipulrahman2@gmail.com>
2023-09-14 00:44:24 +0600

    [UPDATE]: Broadcast receiver update with snackbar

--- threadsplashlottie/app/src/main/java/com/classwork/animationsplash/NetworkChangeReceiver.java
+++ threadsplashlottie/app/src/main/java/com/classwork/animationsplash/NetworkChangeReceiver.java
@@ -5,22 +5,45 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.view.View;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class NetworkChangeReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
+        CoordinatorLayout coordinatorLayout = ((MainActivity) context).findViewById(R.id.coordinatorlayout);
         try {
             if (isOnline(context)) {
-                Toast.makeText(context, "Network Available", Toast.LENGTH_LONG).show();
+                Toast.makeText(context, "Internet Available", Toast.LENGTH_LONG).show();
+                showSnackbar(coordinatorLayout, "Internet Available");
             } else {
-                Toast.makeText(context, "Network Not Available", Toast.LENGTH_LONG).show();
+                Toast.makeText(context, "No Internet Available", Toast.LENGTH_LONG).show();
+                showSnackbar(coordinatorLayout, "No Internet Available");
             }
         } catch (Exception e) {
             e.printStackTrace();
             Toast.makeText(context, "Something went wrong", Toast.LENGTH_SHORT).show();
         }
+    }
+
+    private void showSnackbar(CoordinatorLayout coordinatorLayout, String internet_available) {
+        //make all snackbar dismissable
+        Snackbar snackbar = Snackbar.make(coordinatorLayout, internet_available, Snackbar.LENGTH_LONG);
+        snackbar.setAction("Dismiss", new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                snackbar.dismiss();
+            }
+        }).setActionTextColor(coordinatorLayout.getResources().getColor(android.R.color.holo_red_light));
+        snackbar.show();
+
+
     }
 
     private boolean isOnline(Context context) {
@@ -37,4 +60,6 @@ public class NetworkChangeReceiver extends BroadcastReceiver {
 
 
     }
+
+
 }

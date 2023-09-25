73567a78bac7857eecdf1f580bb1e4299d6444be
Syed-Bipul-Rahman <syedbipulrahman2@gmail.com>
2023-09-26 02:25:54 +0600

    design issue fix

--- Retrofit with Products/app/src/main/java/com/classjob/retrofitwithpicasso/MainActivity.java
+++ Retrofit with Products/app/src/main/java/com/classjob/retrofitwithpicasso/MainActivity.java
@@ -26,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        error = findViewById(R.id.errordekha);
+
         initializing();
         getdata();
 
